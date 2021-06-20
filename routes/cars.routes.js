90f441dd501b4b1585f9a1d9d8dd9a19b3bcbdef
Голованов Сергей <s.s.golovanov@tmn3.etagi.com>
2021-06-20 18:27:29 +0500

    fix

--- routes/cars.routes.js
+++ routes/cars.routes.js
@@ -6,7 +6,7 @@ const Car = require('../models/Car')
 
 router.get("/getCars", async (req, res) => {
     try {
-        const cars = await Car.find().select("-_id");
+        const cars = await Car.find().select("-__v");
         res.send(cars);
     }
     catch (e) {
